266a506930e9138160a1954f4a21bce6ae442349
Agrim Singh <agrim.singh@gmail.com>
2016-01-21 11:31:51 +0800

    8gag
    meme app commit

---
+++ 8gag/Meme.swift
@@ -0,0 +1,15 @@
+//
+//  Meme.swift
+//  8gag
+//
+//  Created by Agrim Singh on 20/1/16.
+//  Copyright © 2016 Agrim Singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var title:String = "default title"
+    var image:UIImage! = UIImage(named: "")
+}
--- 8gag/ViewController.swift
+++ 8gag/ViewController.swift
@@ -13,13 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        prepareMeme()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    var memeList:[Meme] = []
+    
+    
 
+    @IBAction func nextButtonPressed(sender: UIButton) {
+        let selectedMeme = memeList[Int(arc4random_uniform(UInt32(memeList.count)))]
+        titleLabel.text = selectedMeme.title
+        memeImageView.image = selectedMeme.image
+    }
+    
+    func prepareMeme() {
+        for var i = 0; i <= 8; ++i {
+            var memeInstance:Meme = Meme()
+            memeInstance.title = "Image \(i)"
+            memeInstance.image = UIImage(named: "Meme \(i).jpg")
+            memeList.append(memeInstance)
+        }
+    }
 
 }
 
